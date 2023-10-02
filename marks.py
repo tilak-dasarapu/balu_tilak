4f81976d9a57eda7d69f0914f98fbea8a1520cb1
tilak-dasarapu <144981332+tilak-dasarapu@users.noreply.github.com>
2023-10-02 19:01:26 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,19 @@
+marks =35
+if(marks<=100):
+    
+    if(marks >=90):
+        print("Distinction ")
+    elif(marks <90 and marks >=80):
+        print("grade A")
+    elif(marks <80 and marks >=60):
+        print("grade B")
+    elif(marks <60 and marks >=40):
+        print("grade C")
+    elif(marks <40 and marks >=35):
+        print("grade D")      
+    else:
+        print("fail")
+else:
+    print("invalid number")
+        
+
