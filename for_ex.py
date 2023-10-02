7d5a13686f296612ea975dd68e8ab5abcbd64190
tilak-dasarapu <144981332+tilak-dasarapu@users.noreply.github.com>
2023-10-02 19:00:28 +0530

    Add files via upload

---
+++ for_ex.py
@@ -0,0 +1,67 @@
+# for x in range(10,25,3):
+#     print(x)
+
+# for x in range(1,11):
+#     if x == 6:
+#         break
+#     print(x)
+
+# for x in range(1,11):
+#     if x == 6:
+#         continue
+#     print(x)
+
+# for x in range(1,31):
+#     if (x%3==0) and (x%5==0):
+#         print(x)
+
+# for x in range(1,6):
+#     print("sai")
+
+# for x in range(1,11):
+#     if x%2==0:
+#         print("Hi")
+
+#     elif(x%3==0):
+#         print("Hello")
+#     else:
+#         print(x)
+
+# for x in range(1,31):
+#     if (x%3==0) and (x%5==0):
+#         print("fizz buzz")
+#     elif (x%3==0):
+#         print("fizz")
+#     elif (x%5==0):
+#         print("buzz")
+#     else:
+#         print(x)
+
+############### While Examples ##################
+# i = 1  #step1
+
+
+# while(i<=10): #step2  1<=10
+#          print(i)
+#          i = i+2 # i = 1+1 = 2
+        
+
+
+# for i in range (2,50,2):
+
+#     print(i)
+    
+# for x in range(1,101):
+#      if (x%2==0) and (x%4==0):
+#       print("tilak")
+#      else:
+#         print(x)
+
+
+
+
+
+
+
+
+
